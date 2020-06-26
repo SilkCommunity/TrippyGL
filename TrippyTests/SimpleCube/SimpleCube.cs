377dbb1155aecdbab803fbcfdd700e545ce379f8
ThomasMiz <32400648+ThomasMiz@users.noreply.github.com>
2020-06-26 16:28:37 -0300

    Updated NuGet packages & moved test projects to folder

---
+++ TrippyTests/SimpleCube/SimpleCube.cs
@@ -0,0 +1,120 @@
+﻿using Silk.NET.Input;
+using Silk.NET.Input.Common;
+using Silk.NET.OpenGL;
+using Silk.NET.Windowing;
+using Silk.NET.Windowing.Common;
+using System;
+using TrippyGL;
+
+namespace SimpleCube
+{
+    class SimpleCube
+    {
+        IWindow window;
+        IInputContext inputContext;
+
+        GraphicsDevice graphicsDevice;
+
+        public SimpleCube()
+        {
+            window = CreateWindow();
+
+            window.Load += OnWindowLoad;
+            window.Update += OnWindowUpdate;
+            window.Render += OnWindowRender;
+            window.Resize += OnWindowResized;
+            window.Closing += OnWindowClosing;
+        }
+
+        private IWindow CreateWindow()
+        {
+            GraphicsAPI graphicsApi = new GraphicsAPI(ContextAPI.OpenGL, ContextProfile.Core, ContextFlags.Debug, new APIVersion(3, 3));
+            VideoMode videoMode = new VideoMode(new System.Drawing.Size(1280, 720));
+            ViewOptions viewOpts = new ViewOptions(true, 60.0, 60.0, graphicsApi, VSyncMode.Adaptive, 30, false, videoMode, 8);
+            return Window.Create(new WindowOptions(viewOpts));
+        }
+
+        public void Run()
+        {
+            window.Run();
+        }
+
+        private void OnWindowLoad()
+        {
+            inputContext = window.CreateInput();
+            inputContext.Keyboards[0].KeyDown += OnKeyDown;
+            inputContext.Mice[0].MouseDown += OnMouseDown;
+            inputContext.Mice[0].MouseUp += OnMouseUp;
+            inputContext.Mice[0].MouseMove += OnMouseMove;
+
+            graphicsDevice = new GraphicsDevice(GL.GetApi(window));
+            graphicsDevice.DebugMessagingEnabled = true;
+            graphicsDevice.DebugMessage += Program.OnDebugMessage;
+
+            Console.WriteLine(string.Concat("GL Version: ", graphicsDevice.GLMajorVersion, ".", graphicsDevice.GLMinorVersion));
+            Console.WriteLine("GL Version String: " + graphicsDevice.GLVersion);
+            Console.WriteLine("GL Vendor: " + graphicsDevice.GLVendor);
+            Console.WriteLine("GL Renderer: " + graphicsDevice.GLRenderer);
+            Console.WriteLine("GL ShadingLanguageVersion: " + graphicsDevice.GLShadingLanguageVersion);
+            Console.WriteLine("GL TextureUnits: " + graphicsDevice.MaxTextureImageUnits);
+            Console.WriteLine("GL MaxTextureSize: " + graphicsDevice.MaxTextureSize);
+            Console.WriteLine("GL MaxSamples: " + graphicsDevice.MaxSamples);
+
+
+
+            OnWindowResized(window.Size);
+        }
+
+        private void OnWindowUpdate(double dtSeconds)
+        {
+            GLEnum c;
+            while ((c = graphicsDevice.GL.GetError()) != GLEnum.NoError)
+            {
+                Console.WriteLine("Error found: " + c);
+            }
+        }
+
+        private void OnWindowRender(double dtSeconds)
+        {
+            if (window.IsClosing)
+                return;
+
+
+
+            window.SwapBuffers();
+        }
+
+        private void OnMouseMove(IMouse sender, System.Drawing.PointF position)
+        {
+
+        }
+
+        private void OnMouseUp(IMouse sender, MouseButton btn)
+        {
+
+        }
+
+        private void OnMouseDown(IMouse sender, MouseButton btn)
+        {
+
+        }
+
+        private void OnKeyDown(IKeyboard sender, Key key, int idk)
+        {
+
+        }
+
+        private void OnWindowResized(System.Drawing.Size size)
+        {
+            if (size.Width == 0 || size.Height == 0)
+                return;
+
+            graphicsDevice.SetViewport(0, 0, (uint)size.Width, (uint)size.Height);
+        }
+
+        private void OnWindowClosing()
+        {
+            graphicsDevice.Dispose();
+        }
+    }
+}
